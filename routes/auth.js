156f5db56aa2fda47da360b08a6a32170b3a8593
Sowmyam22 <msowmyadeveloper@gmail.com>
2021-06-22 10:22:15 +0530

    custom validator to match the password and confirm password fields

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const {
   getLogin,
@@ -23,15 +23,27 @@ router.get('/signup', getSignup);
 router.post('/login', postLogin);
 
 router.post('/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email!!')
-    .custom((value, {req}) => {                               // custom validator
-      if (value == 'test@test.com') {
-        throw new Error('This email address is forbidden')
-      }
-      return true;
-    }),
+  [  //groups all the checks
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email!!')
+      .custom((value, { req }) => {                               // custom validator
+        if (value == 'test@test.com') {
+          throw new Error('This email address is forbidden')
+        }
+        return true;
+      }),
+    body('password', 'Please enter a password with only numbers and text and at least 5 characters')
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword') //custom validator to match password and confirm password
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!!!');
+        }
+        return true;
+      })
+  ],
   postSignup
 );
 
